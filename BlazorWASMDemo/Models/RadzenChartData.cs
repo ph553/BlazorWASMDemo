495214b1048553a278ab5155b6a1414a35398c22
Zak <zbent@DESKTOP-F9UR5CR>
2022-10-30 09:21:59 -0400

    made radzendatachart model

---
+++ BlazorWASMDemo/Models/RadzenChartData.cs
@@ -0,0 +1,10 @@
+﻿using AntDesign;
+
+namespace BlazorWASMDemo.Models
+{
+    public class RadzenChartData
+    {
+        public string Category { get; set; } = default!;
+        public double Value { get; set; }
+    }
+}
