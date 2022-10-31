6af9afb5e3ea84770ccb704ac9f3da89bbd74d50
Zak <zbent@DESKTOP-F9UR5CR>
2022-10-31 14:58:22 -0400

    cleaned up

---
+++ BlazorWASMDemo/Models/Tank.cs
@@ -0,0 +1,22 @@
+﻿using AntDesign;
+
+namespace BlazorWASMDemo.Models
+{
+    public class Tank
+    {
+        public string Color { get; set; } = "";
+
+        public int TankNumber { get; set; }
+
+        public string Product { get; set; } = "";
+
+        public string Inventory { get; set; } = "";
+
+        public string Capacity { get; set; } = "";
+
+        public string Gravity { get; set; } = "";
+
+        public string PPG { get; set; } = "";
+
+    }
+}
